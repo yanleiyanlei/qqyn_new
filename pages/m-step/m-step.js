5d09aac6b33ce83fc2a26ef542481a31e912f527
taibaoshen <724514563@qq.com>
2019-03-06 17:42:20 +0800

    拼步距离

--- pages/m-step/m-step.js
+++ pages/m-step/m-step.js
@@ -807,6 +807,36 @@ Page({
   //     })
   //   }
   // },
+
+  GPSsubmit: function (e) {
+    wx.getLocation({
+      type: 'gcj02',
+      success: (res) => {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        var latitude2 = 39.784670
+        var longitude2 = 116.332980
+        var rad1 = latitude * Math.PI / 180.0;
+        var rad2 = latitude2 * Math.PI / 180.0;
+        var a = rad1 - rad2;
+        var b = longitude * Math.PI / 180.0 - longitude2 * Math.PI / 180.0;
+        var r = 6378137;
+        var distance = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(rad1) * Math.cos(rad2) * Math.pow(Math.sin(b / 2), 2)));
+        distance = distance * 6378.137;//地球半径
+        distance = Math.round(distance * 10000) / 10000;
+        this.setData({ latitude: latitude, longitude: longitude })
+        wx.showModal({
+          title: '当前位置距青青优农生鲜超市',
+          content:  distance + '公里',
+        })
+      }
+
+    })
+  },
+
+
   Submit2: function (e) {//点击不同的战队显示
     var that = this;
     var zl = e.detail.value.zl;
