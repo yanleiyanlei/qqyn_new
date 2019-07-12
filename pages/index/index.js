be993183a097e4bb6e6ccc4b730783066cebc891
baixiaosheng <765093000@qq.com>
2019-07-12 09:28:21 +0800

    style

--- pages/index/index.js
+++ pages/index/index.js
@@ -754,7 +754,6 @@ Page({
     })
   },
   onShow: function() {
-    console.log(this.getGreatCircleDistance(39.790306,116.339646,39.85856,116.28616));
     this.setData({
       dailySpikeIndex: 0
     })
@@ -889,32 +888,4 @@ Page({
       }
     }
   },
-
-
-
-
-
-
-
-  
-
- 
-  getGreatCircleDistance: function (lat1, lng1, lat2, lng2) {
-    var EARTH_RADIUS = 6378137.0;
-    var PI = Math.PI;
-    function getRad(d) {
-      return d * PI / 180.0;
-    }
-    var radLat1 = getRad(lat1);
-    var radLat2 = getRad(lat2);
-
-    var a = radLat1 - radLat2;
-    var b = getRad(lng1) - getRad(lng2);
-
-    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
-    s = s * EARTH_RADIUS;
-    s = Math.round(s * 10000) / 10000.0;
-
-    return s;
-  }
 })
